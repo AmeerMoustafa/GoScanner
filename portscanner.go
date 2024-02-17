7b7ab01fb7d5057b7120de9bdc416f210ff8a396
Ameer-M <t3hdamnedone@gmail.com>
2024-02-17 12:06:57 +0200

    Implemented waitgroups

--- portscanner.go
+++ portscanner.go
@@ -4,32 +4,31 @@ import (
 	"log"
 	"net"
 	"strconv"
+	"sync"
 	"time"
 )
 
 var ip_to_scan = "192.168.1.1"
 var min_port = 1
 var max_port = 1024
+var wg sync.WaitGroup
 
 func main() {
-	active_threads := 0
-	done_channel := make(chan bool)
 
 	for port := min_port; port <= max_port; port++ {
-		go testTCPConnection(ip_to_scan, port, done_channel) // go threads
-		active_threads++
+		wg.Add(1)
+		go testTCPConnection(ip_to_scan, port) // go threads
 	}
 
-	for active_threads > 0 {
-		<-done_channel
-		active_threads--
-	}
+	wg.Wait()
+	
 }
 
-func testTCPConnection(ip string, port int, done_channel chan bool) {
+func testTCPConnection(ip string, port int) {
+	defer wg.Done()
 	_, err := net.DialTimeout("tcp", ip+":"+strconv.Itoa(port), time.Second*10)
 	if err == nil {
 		log.Printf("Host %s has open port: %d\n", ip, port)
 	}
-	done_channel <- true
+	
 }
